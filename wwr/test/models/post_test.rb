7fb97216fcccd3622ee14d1735545dba9328079f
Avner Shanan <avner.shanan@devbootcamp.com>
2017-05-09 15:46:39 -0500

    Add model tests for Post#recent

--- wwr/test/models/post_test.rb
+++ wwr/test/models/post_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Post#recent returns posts ordered in descending order' do
+    posts = Post.recent
+
+    previous_id = posts[0].id
+    posts[1..-1].each do |post|
+      assert post.id < previous_id, "Post with id #{post} should not come before post with id #{previous_id}"
+      previous_id = post.id
+    end
+  end
+
+  test 'Post#recent returns five posts' do
+    posts = Post.recent
+    assert_equal 5, posts.count, 'Post#recent should return five posts'
+  end
 end
