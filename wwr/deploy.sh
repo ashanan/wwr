e40c35ed70bef9f1d22776c0c96f091fc726fdbc
Avner Shanan <avner.shanan@devbootcamp.com>
2017-08-23 10:21:33 -0500

    Add rvm call to deploy script

--- wwr/deploy.sh
+++ wwr/deploy.sh
@@ -1,20 +1,4 @@
-\# Load RVM into a shell session *as a function*
-if [[ -s "$HOME/.rvm/scripts/rvm" ]] ; then
-
-  \# First try to load from a user install
-  source "$HOME/.rvm/scripts/rvm"
-
-elif [[ -s "/usr/local/rvm/scripts/rvm" ]] ; then
-
-  \# Then try to load from a root install
-  source "/usr/local/rvm/scripts/rvm"
-
-else
-
-  printf "ERROR: An RVM installation was not found.\n"
-
-fi
-
+rvm 2.4.0
 cd /var/www/wwr 
 bundle install --local
 bundle exec rails s -p 9292
