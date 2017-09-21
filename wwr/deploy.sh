78181f121a01dd6e0a7125c4d123abea05f2e352
Avner Shanan <avner.shanan@devbootcamp.com>
2017-09-21 15:23:17 -0500

    Overwrite database.yml as part of the deploy

--- wwr/deploy.sh
+++ wwr/deploy.sh
@@ -4,5 +4,13 @@ echo $PATH
 cd /var/www/wwr 
 rbenv local 2.4.0
 bundle install --local
+
+echo 'production:
+  adapter: sqlite3
+  pool: 5
+  timeout: 5000
+  database: db/production.sqlite3
+' > config/database.yml
+
 kill `cat tmp/pids/server.pid`
 bundle exec rails s -p 9292 -d
