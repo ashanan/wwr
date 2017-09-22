dacafd217cf01a7e9f8cd0f56d8a746857708c27
Avner Shanan <avner.shanan@devbootcamp.com>
2017-09-22 12:31:36 -0500

    Add about page

--- wwr/config/routes.rb
+++ wwr/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'posts/index'
   get 'posts/:id' => 'posts#show', as: 'post'
+  get 'about' => 'about#index', as: 'about'
   root 'posts#index'
 end
