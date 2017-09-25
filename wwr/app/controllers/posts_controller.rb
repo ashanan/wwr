53019e5d49f64377e31a7b72cfbc35ac8f9e2770
Avner Shanan <avner.shanan@devbootcamp.com>
2017-09-25 16:52:04 -0500

    Add basic auth protected form for new posts

--- wwr/app/controllers/posts_controller.rb
+++ wwr/app/controllers/posts_controller.rb
@@ -5,4 +5,33 @@ class PostsController < ApplicationController
 
   def show
   end
+
+  def new
+    basic_http_authentication
+
+    @post = Post.new
+  end
+
+  def create
+    basic_http_authentication
+    
+    post = Post.new(post_params)
+    if post.save
+      redirect_to '/'
+    else
+      @errors = post.errors.full_messages
+      render post
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :original_title, :body, :amazon_us_url, :amazon_uk_url, :original_url)
+  end
+
+  def basic_http_authentication
+    authenticate_or_request_with_http_basic do |user,pass| 
+      user == ENV["WWR_ADMIN_USERNAME"] && pass = ENV["WWR_ADMIN_PASSWORD"] 
+    end 
+  end
 end
--- wwr/config/routes.rb
+++ wwr/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'posts/index'
-  get 'posts/:id' => 'posts#show', as: 'post'
+  # get 'posts/index'
+  # get 'posts/new' => 'posts#new', as: 'new_post'
+  # post 'posts'    => 'posts#create', as: 'create_post'
+  # get 'posts/:id' => 'posts#show', as: 'posts'
+
+  resources :posts, only: [:index, :new, :create, :show]
+ 
+
   get 'about' => 'about#index', as: 'about'
   root 'posts#index'
 end
